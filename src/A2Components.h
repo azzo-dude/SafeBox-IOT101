10176fe112e0cfbb6b06515df5ddf7e51f177cdd
Nguyễn Quý Ngọc <76089776+azzo-dude@users.noreply.github.com>
2025-07-09 22:48:22 +0700

    Update A2Components.h

--- src/A2Components.h
+++ src/A2Components.h
@@ -1,26 +1,40 @@
-#ifndef A2DRIVER_H
-#define A2DRIVER_H
-
-#include <Keypad.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include "Arduino.h"
-#include <Adafruit_GFX.h>
-
-// --- Led Class ---
 class Led {
   private:
     uint8_t _pin;
-    bool    _isConfigured = false;
-  public:
-    Led() : _pin(255), _isConfigured(false) {}
-    Led(uint8_t pin) { setPin(pin); _isConfigured = true; }
-    uint8_t getPin() const;
-    void setPin(uint8_t pin);
-    bool isConfigured() const;
-    void on();
-    void off();
-    void blink(uint16_t ms);
-};
 
-#endif
+  public:
+    Led(uint8_t pin) : _pin(pin) {
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    }
+
+    uint8_t getPin() const {
+      return _pin;
+    }
+
+    void setPin(uint8_t pin) {
+      _pin = pin;
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    }
+
+    void on() {
+      digitalWrite(_pin, HIGH);
+    }
+
+    void off() {
+      digitalWrite(_pin, LOW);
+    }
+
+    void blink(uint16_t ms) {
+      static unsigned long previousMillis = 0;
+      static bool ledState = false;
+
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= ms) {
+        ledState = !ledState;
+        digitalWrite(_pin, ledState ? HIGH : LOW);
+        previousMillis = currentMillis;
+      }
+    }
+};
