06ed09ccb7014dc122740fb40cd7f336a5ef1edd
Nguyễn Quý Ngọc <76089776+azzo-dude@users.noreply.github.com>
2025-07-09 22:57:54 +0700

    Update A2Components.h

--- src/A2Components.h
+++ src/A2Components.h
@@ -42,3 +42,39 @@ class Led {
       }
     }
 };
+
+class Buzzer {
+  private:
+    uint8_t _pin;
+
+  public:
+    Led(uint8_t pin) : _pin(pin) {
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    }
+
+    uint8_t getPin() const {
+      return _pin;
+    }
+
+    void setPin(uint8_t pin) {
+      _pin = pin;
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    }
+
+    void tone(int16_t hz) {
+      tone(_pin, hz);
+    }
+
+    void tone(int16_t hz, int64_t dur) {
+      tone(_pin, hz, dur);
+    }
+
+    void noTone() {
+      noTone(_pin);
+    }
+};
+
+
+#endif
